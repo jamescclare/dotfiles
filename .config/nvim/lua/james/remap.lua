1909f5c9a1f538e4c9d4ec63cef42dd68d48c7e4
James Clare <james@james-work.local>
2024-01-24 14:28:53 -0700

    Cleanup

--- .config/nvim/lua/james/remap.lua
+++ .config/nvim/lua/james/remap.lua
@@ -14,8 +14,3 @@ vim.keymap.set("n", "<Down>", "<nop>")
 vim.keymap.set("n", "<Left>", "<nop>")
 vim.keymap.set("n", "<Right>", "<nop>")
 
--- vim.keymap.set("i", "<Up>", "<nop>")
--- vim.keymap.set("i", "<Down>", "<nop>")
--- vim.keymap.set("i", "<Left>", "<nop>")
--- vim.keymap.set("i", "<Right>", "<nop>")
-
