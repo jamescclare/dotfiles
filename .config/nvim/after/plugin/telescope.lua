3fc7844623cfba11d29f106bed8dcf6be9425499
James Clare <james@james-work.local>
2024-01-24 13:03:47 -0700

    Improved telescope and tmux key bindings

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -1,7 +1,18 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ps', function ()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") });
-end)
 
+local find_hidden_files = function () 
+    builtin.find_files {
+        find_command = { 'rg', '--files', '--iglob', '!.git', '--hidden', '--no-ignore' },
+    }
+end
+
+local grep_input_string = function ()
+	builtin.grep_string({ search = vim.fn.input("Grep > ") });
+end
+
+vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+vim.keymap.set('n', '<leader>ph', find_hidden_files, {})
+vim.keymap.set('n', '<leader>pg', builtin.git_files, {})
+vim.keymap.set('n', '<leader>pl', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>ps', grep_input_string, {})
+vim.keymap.set('n', '<leader>pS', builtin.grep_string, {})
