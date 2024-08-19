60c2bb5b8bc2720d63493689c1db7b36a5000b92
James Clare <james@james-work.local>
2024-08-19 13:46:46 +1000

    remove unused packages

--- .config/nvim/lua/james/packer.lua
+++ .config/nvim/lua/james/packer.lua
@@ -10,12 +10,6 @@ return require('packer').startup(function(use)
     use 'nvim-lua/plenary.nvim'
 
     use {
-        'ThePrimeagen/harpoon',
-        branch = "harpoon2",
-        requires = { {"nvim-lua/plenary.nvim"} }
-    }
-
-    use {
         'nvim-telescope/telescope.nvim', tag = '0.1.5',
         -- or                            , branch = '0.1.x',
         requires = {{'nvim-lua/plenary.nvim'} }
@@ -69,8 +63,4 @@ return require('packer').startup(function(use)
     use 'goolord/alpha-nvim'
 
     use 'numToStr/Comment.nvim'
-
-    use 'folke/which-key.nvim'
-
-    use 'ggandor/leap.nvim'
 end)
