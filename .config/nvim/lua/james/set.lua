dfcfba8e7e48840eb7dfdcee4d131ae87bff4138
James Clare <james@jclare.dev>
2025-05-05 13:05:52 +1000

    bring back inline error messages

--- .config/nvim/lua/james/set.lua
+++ .config/nvim/lua/james/set.lua
@@ -25,3 +25,11 @@ vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
+
+vim.diagnostic.config({
+  virtual_text = true,
+  signs = true,
+  underline = true,
+  update_in_insert = false,
+  severity_sort = true,
+})
