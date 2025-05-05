251496da9c50562d53bd5646173cbb8f8b68e5f7
James Clare <james@jclare.dev>
2025-05-05 13:12:49 +1000

    add some more useful greppers from the work branch

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -15,3 +15,5 @@ vim.keymap.set('n', '<leader>ph', find_hidden_files, { desc = "Find hidden files
 vim.keymap.set('n', '<leader>pg', builtin.git_files, { desc = "Find git files" })
 vim.keymap.set('n', '<leader>pl', builtin.live_grep, { desc = "Live grep" })
 vim.keymap.set('n', '<leader>ps', grep_input_string, { desc = "Grep for string" })
+vim.keymap.set('n', '<leader>po', builtin.oldfiles, { desc = "File history" })
+vim.keymap.set('n', '<leader>pb', builtin.buffers, { desc = "Open buffers" })
